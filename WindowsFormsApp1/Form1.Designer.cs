f24792c43793b7e4d8690cdc9ea6c5f7028c2810
KOSTA <KOSTA@DESKTOP-18JVT33>
2025-05-15 14:04:46 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.today_temp = new System.Windows.Forms.Label();
+            this.weather_icon = new System.Windows.Forms.Panel();
+            this.weather_icon_des = new System.Windows.Forms.Label();
+            this.position = new System.Windows.Forms.Label();
+            this.celsius_text = new System.Windows.Forms.Label();
+            this.weatherTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.weather_icon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // today_temp
+            // 
+            this.today_temp.AutoSize = true;
+            this.today_temp.Font = new System.Drawing.Font("맑은 고딕", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.today_temp.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.today_temp.Location = new System.Drawing.Point(43, 43);
+            this.today_temp.Name = "today_temp";
+            this.today_temp.Size = new System.Drawing.Size(107, 50);
+            this.today_temp.TabIndex = 1;
+            this.today_temp.Text = "TEST";
+            this.today_temp.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // weather_icon
+            // 
+            this.weather_icon.Controls.Add(this.weather_icon_des);
+            this.weather_icon.Location = new System.Drawing.Point(41, 102);
+            this.weather_icon.Name = "weather_icon";
+            this.weather_icon.Size = new System.Drawing.Size(113, 108);
+            this.weather_icon.TabIndex = 2;
+            // 
+            // weather_icon_des
+            // 
+            this.weather_icon_des.AutoSize = true;
+            this.weather_icon_des.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.weather_icon_des.Location = new System.Drawing.Point(7, 44);
+            this.weather_icon_des.Name = "weather_icon_des";
+            this.weather_icon_des.Size = new System.Drawing.Size(96, 21);
+            this.weather_icon_des.TabIndex = 0;
+            this.weather_icon_des.Text = "날씨 아이콘";
+            // 
+            // position
+            // 
+            this.position.AutoSize = true;
+            this.position.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.position.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.position.Location = new System.Drawing.Point(663, 52);
+            this.position.Name = "position";
+            this.position.Size = new System.Drawing.Size(94, 37);
+            this.position.TabIndex = 3;
+            this.position.Text = "label1";
+            // 
+            // celsius_text
+            // 
+            this.celsius_text.AutoSize = true;
+            this.celsius_text.Font = new System.Drawing.Font("맑은 고딕", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.celsius_text.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.celsius_text.Location = new System.Drawing.Point(156, 42);
+            this.celsius_text.Name = "celsius_text";
+            this.celsius_text.Size = new System.Drawing.Size(57, 50);
+            this.celsius_text.TabIndex = 4;
+            this.celsius_text.Text = "℃";
+            this.celsius_text.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // weatherTimer
+            // 
+            this.weatherTimer.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.celsius_text);
+            this.Controls.Add(this.position);
+            this.Controls.Add(this.weather_icon);
+            this.Controls.Add(this.today_temp);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.weather_icon.ResumeLayout(false);
+            this.weather_icon.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label today_temp;
+        private System.Windows.Forms.Panel weather_icon;
+        private System.Windows.Forms.Label weather_icon_des;
+        private System.Windows.Forms.Label position;
+        private System.Windows.Forms.Label celsius_text;
+        private System.Windows.Forms.Timer weatherTimer;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+using RestKit;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WindowsFormsApp1;
+using Microsoft.Win32;
+using System.Runtime.Remoting.Messaging;
+
+
+namespace WindowsFormsApp1
+{
+    public class WeatherInfo
+    {
+        public WeatherInfo()
+        {
+
+        }
+
+        public string[] RunWeather()
+        {
+            return GetWeather().Result;
+        }
+        static async Task<string[]> GetWeather()
+        {
+            RegistryKey reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\my_weather");
+            string ApiKey = reg.GetValue("my_key").ToString();
+
+            dynamic res = new JObject();
+
+            Task<string[]> position = GetPosition();
+            string lat = position.Result[0];
+            string lon = position.Result[1];
+            var client = new RestClient();
+            var request = new RestRequest("https://api.openweathermap.org/data/2.5/weather?lat="
+                + lat
+                + "&lon="
+                + lon
+                + "&appid="
+                + ApiKey
+                + "&units=metric&lang=kr");
+
+            string[] ret = new string[6];
+
+            try
+            {
+                var response = await client.GetAsync(request);
+                res = JObject.Parse(response.Content);
+
+                //Console.WriteLine(res["weather"][0]["description"]);
+                ret[0] = res["weather"][0]["description"];
+                ret[1] = res["main"]["temp"];
+                ret[2] = res["main"]["temp_min"];
+                ret[3] = res["main"]["temp_max"];
+                ret[4] = res["main"]["feels_like"];
+                ret[5] = position.Result[2];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return ret;
+        }
+
+        static async Task<string[]> GetPosition()
+        {
+            dynamic res = new JObject();
+
+            string p_ip = GetIp();
+            var client = new RestClient();
+            var request = new RestRequest("http://ip-api.com/json/" + p_ip);
+            string[] ret = new string[3];
+            try
+            {
+                var response = await client.GetAsync(request);
+                res = JObject.Parse(response.Content);
+
+                ret[0] = res["lat"];
+                ret[1] = res["lon"];
+                ret[2] = res["city"];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return ret;
+        }
+
+        static string GetIp()
+        {
+            string p_ip = new WebClient().DownloadString("http://ipinfo.io/ip").Trim();
+
+            return p_ip;
+        }
+
+    }
+
+    public partial class Form1: Form
+    {
+        OpenCvSharp.VideoCapture video;
+        Mat mImage = new Mat(1080, 1920, MatType.CV_8UC3);
+        private string[] info = new string[6];
+        System.Threading.Timer thread;
+        int num;
+
+
+        /*
+            info[0]: description
+            info[1]: temp
+            info[2]: temp_min
+            info[3]: temp_max
+            info[4]: feels_like
+            info[5]: city name
+         */
+
+        public Form1()
+        {
+            InitializeComponent();
+            num = 0;
+            //weatherInfo
+            //WeatherInfo weather = new WeatherInfo();
+            
+            //weatherTimer.Enabled = true;
+            //weatherTimer.Interval = 600000;
+
+            //this.thread = new System.Threading.Timer();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            today_temp.Parent = pictureBox1;
+            //today_temp.Text = info[1];
+
+            today_temp.BackColor = Color.Transparent;
+
+            weather_icon.Parent = pictureBox1;
+            weather_icon_des.Parent = weather_icon;
+            //weather_icon_des.Text = info[0];
+
+            weather_icon_des.BackColor = Color.Transparent;
+
+            position.Parent = pictureBox1;
+            //position.Text = info[5];
+            position.BackColor = Color.Transparent;
+
+            celsius_text.Parent = pictureBox1;
+            celsius_text.BackColor = Color.Transparent;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const int my_padding = 20;
+            
+            //WeatherInfo weather = new WeatherInfo();
+            //this.info = weather.RunWeather();
+            //thread
+
+            //today_temp.Text = info[1];
+            //weather_icon_des.Text = info[0];
+            //position.Text = info[5];
+
+            try
+            {
+                video = new VideoCapture(0);
+                video.FrameWidth = this.Width;
+                video.FrameHeight = this.Height;
+
+                today_temp.Left = my_padding;
+
+                celsius_text.Left = today_temp.Width + my_padding /2 ;
+
+                weather_icon.Left = my_padding + 10;
+                weather_icon.Top = weather_icon.Height + my_padding;
+
+                position.Left = (this.Width - (my_padding * 7 + today_temp.Width)); // 오른쪽 상단 배치
+                today_temp.Text = num.ToString();
+
+                
+            }
+            catch
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            video.Read(mImage);
+            pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mImage);
+            pictureBox1.Size = this.ClientSize;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mImage.Dispose();
+        }
+
+        private void weatherTimer_Event(object sender, EventArgs e)
+        {
+            //today_temp.Text = this.info[1];
+            //weather_icon_des.Text = this.info[0];
+            //position.Text = this.info[5];
+
+            num++;
+            //weatherTimer
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
